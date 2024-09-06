04bf3ae91a5fea7a98a85c1146c7285f3f2daefc
pujit-2005 <pujit.chhabra05@gmail.com>
2024-09-07 03:16:58 +0530

    server.py

--- server.py
+++ server.py
@@ -4,25 +4,25 @@ import json
 import google.generativeai as genai
 import os
 
-# Ensure that the API key is set as an environment variable
+
 os.environ["API_KEY"] = "AIzaSyAOUiha5ea2d93-uZ2kyH1S7yio7qM3uxk"
 api_key = os.environ.get("API_KEY")
 if not api_key:
     raise ValueError("API_KEY environment variable is not set.")
 
-# Configure the API key
+
 genai.configure(api_key=api_key)
 
 def handle_client(client_socket):
     try:
-        # Receive the prompt from the client
+       
         prompt = client_socket.recv(1024).decode('utf-8')
 
-        # Specify the model and generate content
+       
         model = genai.GenerativeModel('gemini-1.5-flash')
         response = model.generate_content(prompt)
 
-        # Send the response back to the client
+        
         client_socket.send(response.text.encode('utf-8'))
     except Exception as e:
         client_socket.send(f"An error occurred: {e}".encode('utf-8'))
